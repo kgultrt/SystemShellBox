07bb977b8a4ed6530de5f28c903e71e887a48214
kgultrt <apple_1145141919@outlook.com>
2025-08-17 06:25:25 +0800

    Clean repositories

--- terminal-emulator/build/intermediates/cxx/Debug/362e6y1d/obj/local/x86_64/objs-debug/termux/termux.o.d
+++
@@ -1,2 +0,0 @@
-/data/data/com.termux/files/home/SystemShellBox/terminal-emulator/build/intermediates/cxx/Debug/362e6y1d/obj/local/x86_64/objs-debug/termux/termux.o: \
-  /data/data/com.termux/files/home/SystemShellBox/terminal-emulator/src/main/jni/termux.c
--- terminal-emulator/build/intermediates/cxx/Release/21115v51/obj/local/x86/objs/termux/termux.o.d
+++
@@ -1,2 +0,0 @@
-/data/data/com.termux/files/home/SystemShellBox/terminal-emulator/build/intermediates/cxx/Release/21115v51/obj/local/x86/objs/termux/termux.o: \
-  /data/data/com.termux/files/home/SystemShellBox/terminal-emulator/src/main/jni/termux.c
